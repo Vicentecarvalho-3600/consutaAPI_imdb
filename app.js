51801012654cbb800d2a4ecafc82aeba289bbce9
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-05-16 20:54:04 -0300

    iniciar projeto

---
+++ app.js
@@ -0,0 +1,9 @@
+const apiKey = "c4f07ff4";
+const imgDefault = "./default_image.png";
+
+
+async function buscaFilme(nomeBusca){
+     const resposta = await fetch(`http://www.omdbapi.com/?t=${nomeBusca}&apikey=${apiKey}`);
+     return resposta.json();
+}
+
