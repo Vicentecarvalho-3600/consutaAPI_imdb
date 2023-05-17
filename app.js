621607ce2e7d75e9b8d556f83d901cedebc45da2
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-05-17 19:06:14 -0300

    implematado funcao de consulta API

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
-const apiKey = "c4f07ff4";
+const nomeBusca = document.querySelector(".input");
+const mensagemErro = document.querySelector("#mensagemErro");
+const botaoBuscar = document.querySelector("#botao_buscar");
+const titulo = document.querySelector("#titulo");
+const ano = document.querySelector("#ano");
+const duracao = document.querySelector("#duracao");
+const genero = document.querySelector("#genero");
+const diretor = document.querySelector("#diretor");
+const atores = document.querySelector("#atores");
+const poster = document.querySelector(".poster");
+const sinopse = document.querySelector("#sinopse");
+const valorAvaliacao = document.querySelector("#valorAvaliacao");
+const apiKey = "37db561";
 const imgDefault = "./default_image.png";
-
 
 async function buscaFilme(nomeBusca){
      const resposta = await fetch(`http://www.omdbapi.com/?t=${nomeBusca}&apikey=${apiKey}`);
      return resposta.json();
 }
 
+botaoBuscar.addEventListener("click", ()=> {
+     limpaCampos();
+     core();
+})
+
+async function core(){
+     const filme = await buscaFilme(nomeBusca.value);
+     defineValores(filme);
+}
+
+function defineValores(filme){
+     titulo.textContent = filme.Title;
+     sinopse.textContent = filme.Plot;
+     ano.textContent = `Year: ${filme.Year}`;
+     duracao.textContent = `Run time: ${filme.Runtime}`;
+     genero.textContent = `Genre: ${filme.Genre}`;
+     atores.textContent = `Actors: ${filme.Actors}`;
+     diretor.textContent = `Director: ${filme.Director}`;
+     poster.setAttribute("src", filme.Poster);
+}
+
+function limpaCampos(){
+     titulo.textContent = "";
+     sinopse.textContent = "";
+     ano.textContent = "";
+     duracao.textContent = "";
+     genero.textContent = "";
+     atores.textContent = "";
+     diretor.textContent = "";
+     poster.setAttribute("src", imgDefault);
+}
